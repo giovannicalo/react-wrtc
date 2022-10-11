e5e4261235817f440c04d90b256aca1c4ed36a22
Giovanni Calò <giovanni.calo@live.it>
2022-10-11 02:39:19 +0100

    Added main module

---
+++ source/index.js
@@ -0,0 +1,7 @@
+import Context from "./context";
+import useStream from "./hook";
+import Player from "./player";
+import Provider from "./provider";
+import Stream from "./stream";
+
+export { Context, Player, Provider, Stream, useStream };
