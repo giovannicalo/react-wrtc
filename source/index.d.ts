d80bce59b3bbb4274b97a9dfb7405120fff82cef
Giovanni Calò <giovanni.calo@live.it>
2022-10-11 03:52:49 +0100

    Added types

---
+++ source/index.d.ts
@@ -0,0 +1,55 @@
+import type { Context, Provider, ReactElement } from "react";
+import type { ViewProps } from "react-view";
+import type ApiClient from "wrtc-ws-api-client";
+
+type Dimensions = {
+	height: number;
+	width: number;
+};
+
+type PlayerProps = ViewProps & {
+	source?: ApiClient.JsonSerializable;
+};
+
+type ProviderProps = {
+	stream: Stream;
+};
+
+type Region = {
+	height: number;
+	id: number;
+	source?: ApiClient.JsonSerializable;
+	width: number;
+	x: number;
+	y: number;
+};
+
+declare class Composer {
+	createRegion(): Region;
+	get height(): number;
+	get regions(): Region[];
+	resizeRegion(region: Region, dimensions: Dimensions): void;
+	setRegionSource(region: Region, source: ApiClient.JsonSerializable): void;
+	removeRegion(region: Region): void;
+	get width(): number;
+}
+
+declare const Context: Context<Stream>;
+
+declare function Player(props: PlayerProps): ReactElement;
+
+declare function Provider(props: ProviderProps): ReactElement;
+
+declare class Stream {
+	constructor(apiUrl: string, apiOptions: ApiClient.Options);
+	get apiClient(): ApiClient;
+	cancelFrameCallback(handle: number): void;
+	close(): void;
+	get composer(): Composer;
+	requestFrameCallback(callback: () => void): number;
+	get video(): HTMLVideoElement;
+}
+
+declare function useStream(): Stream;
+
+export { Context, Player, Provider, Stream, useStream };
