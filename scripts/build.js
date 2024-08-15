f693509832974b3cf363365d646aba0c9001a312
Giovanni Calò <giovanni.calo@live.it>
2024-08-15 02:23:01 +0100

    Renamed build folder

--- scripts/build.js
+++ scripts/build.js
@@ -2,9 +2,9 @@ const { promises: { copyFile, mkdir } } = require("fs");
 const { join } = require("path");
 
 (async () => {
-	await mkdir(join(__dirname, "..", "dist"), { recursive: true });
+	await mkdir(join(__dirname, "..", "build"), { recursive: true });
 	copyFile(
 		join(__dirname, "..", "source", "index.d.ts"),
-		join(__dirname, "..", "dist", "index.d.ts")
+		join(__dirname, "..", "build", "index.d.ts")
 	);
 })();
