56e8f2d412439e8af2913a85ad32fe58f7945125
Giovanni Calò <giovanni.calo@live.it>
2024-08-15 02:20:22 +0100

    Updated Node version

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	presets: [["@babel/env", {
 		bugfixes: true,
 		shippedProposals: true,
-		targets: { node: "22.3.0" }
+		targets: { node: "22.6.0" }
 	}], ["@babel/react", {
 		runtime: "automatic"
 	}]]
