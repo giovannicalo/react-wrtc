e61f62a4d385c4ebfab67c442a85f0a98b7598ef
Giovanni Calò <giovanni.calo@live.it>
2025-05-04 23:42:16 +0100

    Added frame-accurate region-based video scaling

--- source/player/hooks.js
+++ source/player/hooks.js
@@ -1,8 +1,8 @@
 import { useCallback, useEffect, useImperativeHandle, useRef } from "react";
 
-import renderOverlay from "./render-overlay";
 import useStream from "../hook";
 import Statistics from "../statistics";
+import renderOverlay from "./render-overlay";
 
 const useHooks = ({ debug, onRender, ref, source }) => {
 	const canvas = useRef();
@@ -13,15 +13,35 @@ const useHooks = ({ debug, onRender, ref, source }) => {
 	const stream = useStream();
 	statistics.current ||= new Statistics();
 	const render = useCallback(() => {
-		statistics.current?.start();
-		const { height, width, x, y } = region.current;
+		statistics.current.start();
+		const { height, id, width, x, y } = region.current;
+		const { isValid, regions } = stream.metadata || {};
 		context.current.clearRect(0, 0, width, height);
-		context.current.drawImage(stream.video, x, y, width, height, 0, 0, width, height);
+		const coordinates = [x, y, width, height, 0, 0, width, height];
+		if (isValid && regions[id]) {
+			const region = regions[id];
+			let heightRatio = height / region.height;
+			let widthRatio = width / region.width;
+			if (Math.abs(widthRatio - heightRatio) > 0.01) {
+				const ratio = Math.min(widthRatio, heightRatio);
+				heightRatio = ratio;
+				widthRatio = ratio;
+			}
+			coordinates[0] = region.x;
+			coordinates[1] = region.y;
+			coordinates[2] = region.width;
+			coordinates[3] = region.height;
+			coordinates[4] = Math.max(Math.trunc((width - region.width * widthRatio) / 2), 0);
+			coordinates[5] = Math.max(Math.trunc((height - region.height * heightRatio) / 2), 0);
+			coordinates[6] = region.width * widthRatio;
+			coordinates[7] = region.height * heightRatio;
+		}
+		context.current.drawImage(stream.video, ...coordinates);
 		onRender?.(context.current, region.current);
 		if (debug) {
 			renderOverlay(context.current, region.current, stream.statistics, statistics.current);
 		}
-		statistics.current?.end();
+		statistics.current.end();
 	}, [debug, onRender, stream]);
 	const renderLoop = useCallback(() => {
 		render();
